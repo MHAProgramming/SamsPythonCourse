c918050af37de8858f3a4b102253ea7db86e4801
MHAProgramming <54852888+MHAProgramming@users.noreply.github.com>
2024-10-22 22:54:39 -0400

    Add files via upload
    Hour 3 Excercise on if, elif and else statements.

---
+++ hour_3_ifstatements.py
@@ -0,0 +1,15 @@
+# Excersise
+# Given a number write a snippet of code that will print "You have money" if the number
+# is postitive, "You're out" if it's zero, and "you seem to be in debt" if it's less than zero.
+# Your code should have an if statement, an elif statement, and an else.
+
+# Number Entering
+
+total = 1
+
+if total > 0:
+    print "You have money"
+elif total == 0:
+    print "You're out"
+else:
+    print "You seem to be in debt"
